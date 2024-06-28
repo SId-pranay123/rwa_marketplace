b48004310092e818674aab2edf5db79579c74a1b
Siddharth Pranay <siddharthpranay@Siddharths-Air.bbrouter>
2024-06-28 22:55:38 +0530

    ommited contracts

--- contracts/cw-nft/target/debug/examples/schema-5422443f76f6ca9c.d
+++
@@ -1,5 +0,0 @@
-/Users/siddharthpranay/Desktop/cwNFT/cw-nfts/target/debug/examples/libschema-5422443f76f6ca9c.rmeta: packages/cw721/examples/schema.rs
-
-/Users/siddharthpranay/Desktop/cwNFT/cw-nfts/target/debug/examples/schema-5422443f76f6ca9c.d: packages/cw721/examples/schema.rs
-
-packages/cw721/examples/schema.rs:
--- contracts/cw-nft/target/release/build/generic-array-2a90d03408f3efc5/build_script_build-2a90d03408f3efc5.d
+++
@@ -1,5 +0,0 @@
-/Users/siddharthpranay/Desktop/cwNFT/cw-nfts/target/release/build/generic-array-2a90d03408f3efc5/build_script_build-2a90d03408f3efc5: /Users/siddharthpranay/.cargo/registry/src/index.crates.io-6f17d22bba15001f/generic-array-0.14.7/build.rs
-
-/Users/siddharthpranay/Desktop/cwNFT/cw-nfts/target/release/build/generic-array-2a90d03408f3efc5/build_script_build-2a90d03408f3efc5.d: /Users/siddharthpranay/.cargo/registry/src/index.crates.io-6f17d22bba15001f/generic-array-0.14.7/build.rs
-
-/Users/siddharthpranay/.cargo/registry/src/index.crates.io-6f17d22bba15001f/generic-array-0.14.7/build.rs:
